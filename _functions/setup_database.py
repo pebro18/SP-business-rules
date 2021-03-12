986a3ac9aafc4322069947f349fdb5d72666f187
stanhaakman <stanhaakman@icloud.com>
2021-03-12 10:01:56 +0100

    Add comments in setup_database.py

--- _functions/setup_database.py
+++ _functions/setup_database.py
@@ -7,14 +7,24 @@ from _functions.config import config
 
 
 def create_database(dbname='huwebshop'):
+    '''
+    Function to create a new database and extend the database.ini file.
+    :param dbname:
+    :return:
+    '''
+
+    # Configure parser for database.ini
     parser = ConfigParser()
     parser.read('database.ini')
+
+    # If Database line exists remove it otherwise pass
     try:
         parser.remove_option('postgresql', 'database')
     except ValueError:
         pass
 
     try:
+        # Use config functie to get values from database.ini
         db = config()
         con = psycopg2.connect(**db)
         con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
@@ -28,6 +38,7 @@ def create_database(dbname='huwebshop'):
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
 
+    # Add database name to the database.ini file
     parser['postgresql']['database'] = dbname
     with open('database.ini', 'w') as configFile:
         parser.write(configFile)
@@ -36,9 +47,15 @@ def create_database(dbname='huwebshop'):
 
 
 def fill_database(sqlfile='huwebshop.sql'):
+    '''
+    Fill the database with the database structure from a sql file.
+    :param sqlfile:
+    :return:
+    '''
     db = config()
     con = psycopg2.connect(**db)
     cursor = con.cursor()
+    # Open the given sql file
     with open(sqlfile, 'r') as file:
         content_list = [line.rstrip(';') for line in file]
 
