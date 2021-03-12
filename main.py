def4f8d12d51a90089715475bfde7c0c793bc169
stanhaakman <stanhaakman@icloud.com>
2021-03-12 09:52:11 +0100

    Add comments in main.py

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ converter = Converter()
 converter.products(fieldnames=['_id', 'name', 'brand', 'category', 'deeplink', 'properties.doelgroep', 'fast_mover', 'gender', 'herhaalaankopen', 'price.selling_price'], filename='products.csv')
 
 '''
+Create filter and load in the file. then replace the wanted values.
 
+After that save the new data and print te amount of <null> values in the csv file to check if the filtering process worked.
 '''
 filter_products = Filter_products()
 filter_products.load_dataframe(filename='products.csv')
@@ -24,9 +26,10 @@ filter_products.replace_gender(invalid=['Gezin', 'B2B', 'Kinderen', 'Senior', 'B
 filter_products.save_dataframe()
 print(filter_products.dataframe.isna().sum())
 
-
+# Create and fill the database with the table structure
 create_database()
 fill_database()
 
+# Create sender and query the products
 data_sender = DataSender()
 data_sender.send_products(file='products.csv')
