b22bec584d40b91b47d8f7b3c7eb6a5c8ca47430
P Brouwer <pebro15@hotmail.com>
2021-03-15 12:05:28 +0100

    de pseudocode voor collab en content filtering gemaakt

---
+++ Rules/Collab_rules.py
@@ -0,0 +1,14 @@
+"""
+pseudocode for collab filtering
+
+1. visitors en sessions pakken
+2a type visitor
+2b/ events pakken
+2c/ similars
+2d/ upselling duurdere variant verkopen
+2e/ crossselling aan verwanten producten
+2f/ deepselling meer verkopen aan soort gelijken producten
+2g/ kijken naar sessions over de financieel situatie
+3. linken in koppeltabel similars
+4. recommandaties geven
+"""
