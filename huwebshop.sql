ab9163de836f328fd6764ef46e6f5bb50553c640
P Brouwer <pebro15@hotmail.com>
2021-03-15 14:03:29 +0100

    update sql

--- huwebshop.sql
+++ huwebshop.sql
@@ -23,6 +23,7 @@ CREATE TABLE IF NOT EXISTS Products (
   brand VARCHAR(255) NULL,
   category VARCHAR(255) NULL,
   deeplink VARCHAR(255) NULL,
+  doelgroep VARCHAR(255) NULL,
   fastmover BOOLEAN NULL,
   target genders NULL,
   herhaalaankopen BOOLEAN NULL,
